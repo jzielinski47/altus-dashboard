4ded43663d79420169cadc636925e7b3289dd0bf
Jakub Zieliński <jzielinski47dev@gmail.com>
2024-10-02 11:32:30 +0200

    api_introduction

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,14 @@ import express from "express";
 const app = express();
 const port: number = parseInt(process.env.PORT || "8000", 10);
 
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 0, username: "admin", password: "admin" },
+    { id: 1, username: "user", password: "user" },
+    { id: 2, username: "guest", password: "guest" },
+  ]);
+});
+
 app.listen(port, () => {
-  console.log(`Running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
