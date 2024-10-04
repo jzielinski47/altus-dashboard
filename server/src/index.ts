c9b4776836058c7908186a3004c4509bb92f8343
Jakub Zieliński <jzielinski47dev@gmail.com>
2024-10-04 18:54:47 +0200

    code_refactor

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,45 @@
 import express from "express";
 
 const app = express();
-const port: number = parseInt(process.env.PORT || "8000", 10);
+const port: number = parseInt(process.env.PORT || "3000", 10);
+
+const usersData = [
+  { id: 0, username: "admin", token: "0" },
+  { id: 1, username: "user", token: "1" },
+];
+
+/*
+  My first step is to make a simple log-in/register system working on GET/POST requests only with no web-sockets invloved.
+*/
+
+/*
+200 OK
+201 Created
+202 Accepted
+204 No Content
+400 Bad Request
+401 Unauthorized
+403 Forbidden
+404 Not Found
+*/
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 0, username: "admin", password: "admin" },
-    { id: 1, username: "user", password: "user" },
-    { id: 2, username: "guest", password: "guest" },
-  ]);
-  console.log(req.query);
+  res.send(usersData);
 });
 
 app.get("/api/users/:id", (req, res) => {
   console.log(req.params);
+  const requestedId = parseInt(req.params.id);
+  isNaN(requestedId) ? res.status(400).send({ msg: "Invalid id." }) : null;
+  const filteredUser = usersData.find((user) => user.id === requestedId);
+  !filteredUser ? res.status(404).send({ msg: "No such user." }) : null;
+  res.status(200).send(filteredUser);
 });
 
-app.post("/api/users", (req, res) => {});
+app.post("/api/users", (req, res) => {
+  console.log(req.body);
+  res.status(200);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
