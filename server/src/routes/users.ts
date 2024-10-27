db133121d49be1989f244d24c339e681979a9133
Jakub Zieliński <jzielinski47dev@gmail.com>
2024-10-27 20:01:26 +0100

    user_permission_granted

--- server/src/routes/users.ts
+++ server/src/routes/users.ts
@@ -19,13 +19,58 @@ router.get("/api/users", async (req, res) => {
   res.send(users);
 });
 
-// delete user account
 //@ts-ignore
-router.post("/api/users/delete/:id", authorizeAdmin, (req, res) => {});
+router.post("/api/users/delete/:username", authorizeAdmin, async (req, res) => {
+  const { username } = req.params;
+
+  try {
+    const deletedUser = await User.findOneAndDelete({
+      username,
+    });
+
+    if (!deletedUser) {
+      return res
+        .status(404)
+        .send({ msg: `User ${req.params.username} not found` });
+    }
+
+    res
+      .status(200)
+      .send({ msg: `User ${req.params.username} has been deleted` });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({ msg: "An error occurred while deleting the user" });
+  }
+});
 
 // grant role "user"/"administrator"
 //@ts-ignore
-router.post("/api/users/grant/:id", authorizeAdmin, (req, res) => {});
+router.patch("/api/users/grant/:username", authorizeAdmin, async (req, res) => {
+  try {
+    const updatedUser = await User.findOneAndUpdate(
+      { username: req.params.username },
+      { role: "administrator" },
+      { new: true }
+    );
+    
+    if (!updatedUser) {
+      return res
+        .status(404)
+        .send({ msg: `User ${req.params.username} not found` });
+    }
+    
+    res.status(200).send({
+      msg: `User ${req.params.username} has been granted administrator rights`,
+      user: updatedUser,
+    });
+    
+  } catch (err) {
+    console.error(err);
+    res
+      .status(500)
+      .send({ msg: "An error occurred while updating the user role" });
+  }
+});
 
 //@ts-ignore
 router.get("/api/admin", authorizeAdmin, async (req, res) => {
