83567709b9113ac888fda0b6c354dc56ddca9259
Jakub Zieliński <jzielinski47dev@gmail.com>
2024-10-28 20:53:04 +0100

    frontend_minor_update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Error from "./pages/Error";
 import Header from "./components/Header";
+import About from "./pages/About";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/auth" element={<Login />} />
+            <Route path="/about" element={<About />} />
             <Route path="*" element={<Error />} />
           </Routes>
         </Router>
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,9 @@ const Header = () => {
 
   return (
     <div className="w-full flex flex-row place-content-between items-center">
-      <h2 className="font-semibold text-xl">{appName}</h2>
+      <a href="/">
+        <h2 className="font-semibold text-xl">{appName}</h2>
+      </a>
       <a
         href="/auth"
         className="text-sm font-semibold leading-6 text-neutral-400"
