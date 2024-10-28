58149dac16f9b8cfc1588ef745a6c2f9d63326cb
Jakub Zieliński <jzielinski47dev@gmail.com>
2024-10-28 16:50:53 +0100

    frontend_update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,17 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Error from "./pages/Error";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <div className="App bg-neutral-900">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Login />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
-      </Router>
+      <div className="w-screen h-screen flex flex-col items-center bg-neutral-900 text-white py-8 px-8 sm:px-16">
+        <Header />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/auth" element={<Login />} />
+            <Route path="*" element={<Error />} />
+          </Routes>
+        </Router>
+      </div>
     </div>
   );
 };
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,5 +1,17 @@
 const Header = () => {
-  return <></>;
+  const appName: string = `Altus`;
+
+  return (
+    <div className="w-full flex flex-row place-content-between items-center">
+      <h2 className="font-semibold text-xl">{appName}</h2>
+      <a
+        href="/auth"
+        className="text-sm font-semibold leading-6 text-neutral-400"
+      >
+        Log in <span aria-hidden="true">&rarr;</span>
+      </a>
+    </div>
+  );
 };
 
 export default Header;
