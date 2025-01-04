9131697423ce95ce4340e57e5950e1a1f76f652b
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-01-04 10:31:52 +0100

    minor_avatar_integration

--- client/public/avatars.js
+++
@@ -1,6 +0,0 @@
-export const avatar = {
-  default: {
-    male: { 0: "https://api.dicebear.com/9.x/lorelei/svg?seed=Nolan&flip=true&backgroundColor=c0aede" },
-    1: "https://api.dicebear.com/9.x/lorelei/svg?seed=Wyatt&flip=true&backgroundColor=c0aede",
-  },
-};
