9f845254900aa99985165e628d97c63a7dbec90e
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-01-04 15:00:20 +0100

    dist_removed

--- server/dist/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const middlewares_1 = require("../utils/middlewares");
-const router = (0, express_1.Router)();
-router.get("/api/dashboard", middlewares_1.isAuthenticated, (req, res) => {
-    res.send({ msg: "dashboard" });
-});
-exports.default = router;
