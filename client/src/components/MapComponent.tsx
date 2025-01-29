a054b5261a4deee01cdd3e4c4798f73f04e27b50
Jakub Zieliński <jzielinski47dev@gmail.com>
2025-01-29 17:20:08 +0100

    updated_map_coords_render

--- client/src/components/MapComponent.tsx
+++ client/src/components/MapComponent.tsx
@@ -1,6 +1,15 @@
-import { MapContainer, Marker, TileLayer } from "react-leaflet";
+import { MapContainer, Marker, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Icon } from "leaflet";
+import { Icon, LatLngExpression } from "leaflet";
+import { useEffect, useState } from "react";
+
+const UpdateMapCenter = ({ coords }: { coords: LatLngExpression }) => {
+  const map = useMap();
+  useEffect(() => {
+    map.setView(coords);
+  }, [coords, map]);
+  return null;
+};
 
 const EVChargerIcon = new Icon({
   iconUrl: "https://img.icons8.com/?size=100&id=Lkvik7ce5xnz&format=png&color=000000",
@@ -8,14 +17,40 @@ const EVChargerIcon = new Icon({
 });
 
 const MapComponent = () => {
+  const [isLocationPermited, setIsLocationPermited] = useState(true);
+  const [coords, setCoords] = useState<LatLngExpression>([50, 0]);
+
+  const aquireGeolocation = () => {
+    if ("geolocation" in navigator) {
+      setIsLocationPermited(true);
+      navigator.geolocation.watchPosition((pos) => {
+        setCoords([pos.coords.latitude, pos.coords.longitude]);
+      });
+      console.log(coords);
+    } else {
+      setIsLocationPermited(false);
+    }
+  };
+
+  useEffect(() => {
+    aquireGeolocation();
+  }, []);
+
+  useEffect(() => {
+    aquireGeolocation();
+  }, [coords]);
+
+  if (!isLocationPermited) return <div>Location unavailabvle</div>;
+
   return (
-    <MapContainer center={[50, 20]} zoom={13}>
+    <MapContainer center={coords} zoom={13}>
       <div className="w-32 h-32">
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
       </div>
+      <UpdateMapCenter coords={coords} />
       <Marker position={[50.0185, 19.9745]} icon={EVChargerIcon}></Marker>
     </MapContainer>
   );
